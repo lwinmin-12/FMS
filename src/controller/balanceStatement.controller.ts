e5aceefb04c17d01050364b14af6c001a1b40e5a
MAC <mac@LMOs-MacBook-Pro.local>
2024-02-29 10:19:28 +0630

    controller update

---
+++ src/controller/balanceStatement.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+import fMsg from "../utils/helper";
+
+export const getStatementBalance = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+
+export const addReciveBalance = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
--- src/service/balanceStatement.service.ts
+++ src/service/balanceStatement.service.ts
@@ -7,39 +7,23 @@ import balanceStatementModel, {
 export const getTotalBalance = async (
   query: FilterQuery<balanceStatementDocument>
 ) => {
-  try {
     return await balanceStatementModel.find(query).lean();
-  } catch (e) {
-    throw new Error(e);
-  }
 };
 
 export const addTotalBalance = async (body: balanceStatementDocument) => {
-  try {
     return await new balanceStatementModel(body).save();
-  } catch (e) {
-    throw new Error(e);
-  }
 };
 
 export const deleteTotalBalance = async (
   query: FilterQuery<balanceStatementDocument>
 ) => {
-  try {
     return await balanceStatementModel.deleteMany(query);
-  } catch (e) {
-    throw new Error(e);
-  }
 };
 
 export const updateTotalBalance = async (
   query: FilterQuery<balanceStatementDocument>,
   body: UpdateQuery<balanceStatementDocument>
 ) => {
-  try {
     await balanceStatementModel.updateMany(query, body);
     return await balanceStatementModel.find(query).lean();
-  } catch (e) {
-    throw new Error(e);
-  }
 };
